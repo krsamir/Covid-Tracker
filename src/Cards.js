3766fa70d8545e628e929dfdec051213abfec353
krsamir <samir1741012321@gmail.com>
2020-06-19 18:19:15 +0530

    editted cards

--- src/Cards.js
+++ src/Cards.js
@@ -1,46 +1,15 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import styles from './OutlinedCard.module.css'
+import styles from './Cards.module.css'
 import { Grid } from '@material-ui/core';
 
-import Paper from '@material-ui/core/Paper';        //*********************** */
-
-/*const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
-
-
-const useStyles1 = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection:'row',
-    flexWrap: 'wrap',
-     '& > *': {
-      margin: theme.spacing(1),
-      width: theme.spacing(1),
-      height: theme.spacing(25),
-    },
-  },
-}));
-*/
-export default function OutlinedCard() {
+export default function Cards() {
   return (
     <div className={styles.container}>
-      <Grid className={styles.gridbox} container spacing={3} justify="center">
-        <Paper elevation={10}>
-          <Card className={styles.classes} variant="outlined">
+      <Grid className={styles.gridbox}>
+          <Card className={styles.classes}>
             <CardContent>
               <Typography className={styles.title} color="textSecondary" gutterBottom>
                 Infected Cases
@@ -53,11 +22,9 @@ export default function OutlinedCard() {
               </Typography>
             </CardContent>
           </Card>
-        </Paper>
       </Grid>
-      <Grid className={styles.gridbox} container spacing={3} justify="center">
-        <Paper elevation={10}>
-          <Card className={styles.classes} variant="outlined">
+      <Grid className={styles.gridbox}>
+          <Card className={styles.classes}>
             <CardContent>
               <Typography className={styles.title} color="textSecondary" gutterBottom>
                 Active Cases
@@ -70,12 +37,10 @@ export default function OutlinedCard() {
               </Typography>
             </CardContent>
           </Card>
-        </Paper>
       </Grid>
 
-      <Grid className={styles.gridbox} container spacing={3} justify="center">
-        <Paper elevation={10}>
-          <Card className={styles.classes} variant="outlined">
+      <Grid className={styles.gridbox}>
+          <Card className={styles.classes}>
             <CardContent>
               <Typography className={styles.classes} color="textSecondary" gutterBottom>
                 Death Cases
@@ -88,8 +53,6 @@ export default function OutlinedCard() {
               </Typography>
             </CardContent>
           </Card>
-
-        </Paper>
       </Grid>
     </div>
   );
