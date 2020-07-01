0d79d1aac0606c9a4a367e57c7016f9e354473a4
Tenneti Venkat <tennetihemant@gmail.com>
2020-07-02 03:01:46 +0530

    Addition of API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBox from './Components/SearchBox';
-import View from './View'
+import View from './View';
 
 ReactDOM.render(
   <React.StrictMode>
