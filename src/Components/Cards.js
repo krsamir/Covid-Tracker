2e4323537deae65e09461ba9a8bc1402494e2da6
krsamir <samir1741012321@gmail.com>
2020-07-16 22:41:27 +0530

    updated

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,110 +1,141 @@
-import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import styles from './Cards.module.css'
-import CountUp from 'react-countup'
-import { Grid } from '@material-ui/core'; 
-import Cured_cases from '../images/Cured_cases.png';
-import active_cases from '../images/active_cases.png';
-import death_cases from '../images/death_cases.png';
-import migrated_cases from '../images/migrated_cases.png';
-
-export default function Cards({data_cases:{confirmed,recovered,deaths,lastUpdate}}) {
-  if(!confirmed){
-    return (<h1>Loading</h1>);
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import styles from "./Cards.module.css";
+import CountUp from "react-countup";
+import { Grid } from "@material-ui/core";
+import Cured_cases from "../images/Cured_cases.png";
+import active_cases from "../images/active_cases.png";
+import death_cases from "../images/death_cases.png";
+import migrated_cases from "../images/migrated_cases.png";
+import "tachyons";
+export default function Cards({
+  data_cases: { confirmed, recovered, deaths, lastUpdate },
+}) {
+  if (!confirmed) {
+    return <h1>Loading</h1>;
   }
   return (
-    <div className={styles.container}> 
-      <Grid className={styles.gridbox}>
-        <Card className={styles.classes}>
-          <CardContent>
-          <img src={migrated_cases} alt="Confirmed Cases" />
-            <Typography className={styles.title} color="textSecondary" gutterBottom>
-              Confirmed Cases
+    <div>
+      <div className={styles.container}>
+        <Grid className={styles.gridbox}>
+          <Card className={styles.classes}>
+            <CardContent>
+              <img src={migrated_cases} alt="Confirmed Cases" />
+              <Typography
+                className={styles.title}
+                color="textSecondary"
+                gutterBottom
+              >
+                Confirmed Cases
               </Typography>
-            <Typography variant="h5" component="h2">
-              <CountUp
-                start={0}
-                end={confirmed.value}
-                duration={2.0}
-                separator=","
-              />
-            </Typography>
-            <Typography className={styles.pos} color="textSecondary">
-              Last Updated Time
+              <Typography variant="h5" component="h2">
+                <CountUp
+                  start={0}
+                  end={confirmed.value}
+                  duration={2.0}
+                  separator=","
+                />
               </Typography>
-            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid className={styles.gridbox}>
-        <Card className={styles.classes}>
-          <CardContent>
-          <img src={active_cases} alt="Active Cases" />
-            <Typography className={styles.title} color="textSecondary" gutterBottom>
-              Active Cases
+              <Typography className={styles.pos} color="textSecondary">
+                Last Updated Time
               </Typography>
-            <Typography variant="h5" component="h2">
-              <CountUp
-                start={0}
-                end={confirmed.value-recovered.value}
-                duration={2.0}
-                separator=","
-              />
-            </Typography>
-            <Typography className={styles.pos} color="textSecondary">
-              Last Updated Time
+              <Typography color="textSecondary">
+                {new Date(lastUpdate).toDateString()}
               </Typography>
-              <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid className={styles.gridbox}>
-        <Card className={styles.classes}>
-          <CardContent>
-          <img src={Cured_cases} alt="Recovered Cases" />
-            <Typography className={styles.title} color="textSecondary" gutterBottom>
-              Recovered Cases
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid className={styles.gridbox}>
+          <Card className={styles.classes}>
+            <CardContent>
+              <img src={active_cases} alt="Active Cases" />
+              <Typography
+                className={styles.title}
+                color="textSecondary"
+                gutterBottom
+              >
+                Active Cases
               </Typography>
-            <Typography variant="h5" component="h2">
-              <CountUp
-                start={0}
-                end={recovered.value}
-                duration={2.0}
-                separator=","
-              />
-            </Typography>
-            <Typography className={styles.pos} color="textSecondary">
-              Last Updated Time
+              <Typography variant="h5" component="h2">
+                <CountUp
+                  start={0}
+                  end={confirmed.value - recovered.value}
+                  duration={2.0}
+                  separator=","
+                />
               </Typography>
-              <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-          </CardContent>
-        </Card>
-      </Grid>
+              <Typography className={styles.pos} color="textSecondary">
+                Last Updated Time
+              </Typography>
+              <Typography color="textSecondary">
+                {new Date(lastUpdate).toDateString()}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid className={styles.gridbox}>
+          <Card className={styles.classes}>
+            <CardContent>
+              <img src={Cured_cases} alt="Recovered Cases" />
+              <Typography
+                className={styles.title}
+                color="textSecondary"
+                gutterBottom
+              >
+                Recovered Cases
+              </Typography>
+              <Typography variant="h5" component="h2">
+                <CountUp
+                  start={0}
+                  end={recovered.value}
+                  duration={2.0}
+                  separator=","
+                />
+              </Typography>
+              <Typography className={styles.pos} color="textSecondary">
+                Last Updated Time
+              </Typography>
+              <Typography color="textSecondary">
+                {new Date(lastUpdate).toDateString()}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
 
-      <Grid className={styles.gridbox}>
-        <Card className={styles.classes}>
-          <CardContent>
-          <img src={death_cases} alt="Death Cases" />
-            <Typography className={styles.classes} color="textSecondary" gutterBottom>
-              Death Cases
+        <Grid className={styles.gridbox}>
+          <Card className={styles.classes}>
+            <CardContent>
+              <img src={death_cases} alt="Death Cases" />
+              <Typography
+                className={styles.classes}
+                color="textSecondary"
+                gutterBottom
+              >
+                Death Cases
               </Typography>
-            <Typography variant="h5" component="h2">
-              <CountUp
-                start={0}
-                end={deaths.value}
-                duration={2.0}
-                separator=","
-              />
-            </Typography>
-            <Typography className={styles.pos} color="textSecondary">
-              Last Updated Time
+              <Typography variant="h5" component="h2">
+                <CountUp
+                  start={0}
+                  end={deaths.value}
+                  duration={2.0}
+                  separator=","
+                />
               </Typography>
-            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-          </CardContent>
-        </Card>
-      </Grid>
+              <Typography className={styles.pos} color="textSecondary">
+                Last Updated Time
+              </Typography>
+              <Typography color="textSecondary">
+                {new Date(lastUpdate).toDateString()}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        {/* <label className={styles.date_time}>
+        {new Date(lastUpdate).toDateString()}
+      </label> */}
+      </div>
     </div>
   );
 }
--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,91 +1,92 @@
-import React, { Component } from 'react'
-import ReactSearchBox from 'react-search-box'
-import styles from './SearchBox.module.css'; 
-import date from 'date-and-time';
-import Cards from './Cards';
-import {fetchData} from '../api/api';
+import React, { Component } from "react";
+import ReactSearchBox from "react-search-box";
+import styles from "./SearchBox.module.css";
+import date from "date-and-time";
+import Cards from "./Cards";
+import { fetchData } from "../api/api";
+import View from ".././View";
 
-const date_time =()=>{
-    const now = new Date();
-    const time_view=date.format(now, 'YYYY/MM/DD HH:mm:ss');
-    return time_view;    
-}
+const date_time = () => {
+  const now = new Date();
+  const time_view = date.format(now, "YYYY/MM/DD HH:mm:ss");
+  return time_view;
+};
 
-export default class SearchBox extends Component { 
-    data = [
-        {
-            key: 'An',
-            value: 'Andhra Pradesh',
-        },
-        {
-            key: 'Ar',
-            value: 'Arunachal Pradesh',
-        },
-        {
-            key: 'As',
-            value: 'Assam',
-        },
-        {
-            key: 'Bi',
-            value: 'Bihar',
-        },
-        {
-            key: 'Ch',
-            value: 'Chhattisgarh',
-        },
-        {
-            key: 'Go',
-            value: 'Goa'
-        },
-        {
-            key: 'Gu',
-            value: 'Gujrat'
-        },
-        {
-            key: 'Ha',
-            value: 'Haryana'
-        },
-        {
-            key: 'Hi',
-            value: 'Himachal Pradesh'
-        }
-    ]
-    constructor(){
-        super()
-        this.state={	
-            data_cases:{},
-            countryData:{},
-        }
-    }
-    async componentDidMount () {
-        const fetchedData = await fetchData();
-        // const indiaData=await fetchINDIA();
-        this.setState({
-            data_cases: fetchedData,
-            // countryData: indiaData
-        })
-    }
-    render() {
-        const {data_cases}=this.state;
-        return (
-            <div>
-            <div className = {styles.searchbox}>
-                <label className={styles.label}>Search your city</label>
-               <ReactSearchBox
-                placeholder="States"
-                value=""
-                data={this.data}
-                onSelect={record => console.log(record)}
-                inputBoxFontColor = "Red"
-                dropDownHoverColor = "grey"
-            /> 
-            <label className={styles.date_time}>{date_time()}</label>
+export default class SearchBox extends Component {
+  data = [
+    {
+      key: "An",
+      value: "Andhra Pradesh",
+    },
+    {
+      key: "Ar",
+      value: "Arunachal Pradesh",
+    },
+    {
+      key: "As",
+      value: "Assam",
+    },
+    {
+      key: "Bi",
+      value: "Bihar",
+    },
+    {
+      key: "Ch",
+      value: "Chhattisgarh",
+    },
+    {
+      key: "Go",
+      value: "Goa",
+    },
+    {
+      key: "Gu",
+      value: "Gujrat",
+    },
+    {
+      key: "Ha",
+      value: "Haryana",
+    },
+    {
+      key: "Hi",
+      value: "Himachal Pradesh",
+    },
+  ];
+  constructor() {
+    super();
+    this.state = {
+      data_cases: {},
+      countryData: {},
+    };
+  }
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    // const indiaData=await fetchINDIA();
+    this.setState({
+      data_cases: fetchedData,
+      // countryData: indiaData
+    });
+  }
+  render() {
+    const { data_cases } = this.state;
+    return (
+      <div className = 'tc flex flex-wrap justify-center'>
+        <div className={styles.searchbox}>
+          <label className={styles.label}>Search your city</label>
+          <ReactSearchBox
+            placeholder="States"
+            value=""
+            data={this.data}
+            onSelect={(record) => console.log(record)}
+            inputBoxFontColor="Red"
+            dropDownHoverColor="grey"
+          />
+          <label className={styles.date_time}>{date_time()}</label>
 
-            <Cards data_cases={data_cases} /> 
-            {/* countryData={this.state.countryData}/> */}
-            </div>   
-            </div>
-        )
-    }
-    // reference: https://www.npmjs.com/package/react-search-box
+          <Cards data_cases={data_cases} /> 
+          {/* countryData={this.state.countryData}/> */}
+        </div>
+      </div>
+    );
+  }
+  // reference: https://www.npmjs.com/package/react-search-box
 }
